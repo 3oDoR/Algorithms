a4a9a823b2d5c95ef11b6d5c0c29b1cc7946032f
Nikita <smirnov2.nv@gmail.com>
2025-04-03 14:05:17 +0300

    Solved problem №704

---
+++ src/leetcode/704. Binary Search.kt
@@ -0,0 +1,24 @@
+package leetcode
+
+fun main() {
+    println(search(listOf(2,5).toIntArray(), 9))
+}
+
+fun search(nums: IntArray, target: Int): Int {
+    var left = 0
+    var right = nums.size - 1
+    var middle: Int
+
+    while(right >= left) {
+        middle = (right + left) / 2
+        if (nums[middle] == target) {
+            return middle
+        }
+        if (nums[middle] > target) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+    return -1
+}
