de471d69b994da276fd97244bb4e90cba272e23a
Nikita <smirnov2.nv@gmail.com>
2025-04-04 16:25:05 +0300

    Solved problem №35

---
+++ src/leetcode/35. Search Insert Position.kt
@@ -0,0 +1,25 @@
+package leetcode
+
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.size - 1
+        var ans = -1
+        while(left <= right) {
+            val middle = (left + right) / 2
+            if(nums[middle] == target) {
+                return middle
+            }
+            if(nums[middle] > target) {
+                ans = middle
+                right = middle - 1
+            } else {
+                left = middle + 1
+            }
+        }
+        if(ans == -1) {
+            return nums.size
+        }
+        return ans
+    }
+}
