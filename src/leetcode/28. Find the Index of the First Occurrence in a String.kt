58451af031b345608161c822eff029c616f4eb87
Nikita <smirnov2.nv@gmail.com>
2025-04-08 09:56:45 +0300

    Solved problem №28

---
+++ src/leetcode/28. Find the Index of the First Occurrence in a String.kt
@@ -0,0 +1,42 @@
+fun main() {
+    val solution = Solution()
+    println(solution.strStr("mississippi", "sipp"))
+}
+
+class Solution {
+
+    // sadbutsad  sad
+    // sadbutsad  bud
+    // satbutsad  sad
+    // sadbutsad  s
+
+    fun strStr(haystack: String, needle: String): Int {
+        var firstIndex = -1
+
+        if (needle.length > haystack.length) {
+            return firstIndex
+        }
+
+        for (i in haystack.indices) {
+            if (haystack[i] == needle[0]) {
+                val rightHaystackIndex = i + needle.lastIndex
+                if (rightHaystackIndex <= haystack.lastIndex) {
+                    if (haystack[rightHaystackIndex] == needle[needle.length - 1]) {
+                        var firstNeedleIndex = 0
+                        for (j in i..rightHaystackIndex) {
+                            if (haystack[j] == needle[firstNeedleIndex]) {
+                                firstNeedleIndex++
+                            } else {
+                                break
+                            }
+                            if (j == rightHaystackIndex) {
+                                return i
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return -1
+    }
+}
