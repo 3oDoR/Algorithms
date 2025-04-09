1368ea1a4bcdb3cc96499994fc33ccb85e7d21f8
Nikita <smirnov2.nv@gmail.com>
2025-04-09 10:05:07 +0300

    Solved problem №1480

---
+++ src/leetcode/1480. Running Sum of 1d Array.kt
@@ -0,0 +1,11 @@
+package leetcode
+
+class Solution {
+    fun runningSum(nums: IntArray): IntArray {
+        for(i in 1..nums.size - 1) {
+            nums[i] += nums[i - 1]
+        }
+        return nums
+    }
+
+}
